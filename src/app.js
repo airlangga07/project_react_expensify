eab96557b253d65903bba8a5f2413d5961a4d516
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-12-30 15:42:16 +0800

    create react boilerplate and empty the folder

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>This is my boilerplate </p>, document.getElementById('app'));
