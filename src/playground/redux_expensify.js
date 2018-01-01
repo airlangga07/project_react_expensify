0a57de7157ff014a3e63de11b657545e05b501cd
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-01-01 14:02:46 +0800

    10-97: Sorting Redux Data

--- src/playground/redux_expensify.js
+++ src/playground/redux_expensify.js
@@ -126,6 +126,14 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
     const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
     
     return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    }
+
+    if (sortBy === 'amount') {
+      return a.amount > b.amount ? 1 : -1;
+    }
   });
 }
 
@@ -143,21 +151,21 @@ store.subscribe(() => {
   console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 0.30, createdAt: -1000 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -21000 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 0.30, createdAt: 1000 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 0.50 }));
 
-store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter());
 
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
-store.dispatch(setStartDate(0));
+// store.dispatch(setStartDate(0));
 // store.dispatch(setStartDate());
-store.dispatch(setEndDate(999));
+// store.dispatch(setEndDate(999));
 
 const demoState = {
   expenses: [{
