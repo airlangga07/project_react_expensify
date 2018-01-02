770621c122567dee9a1393545894e41cbe3537ae
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-01-02 21:58:02 +0800

    11-103: Controlled Inputs for Filters

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { removeExpense } from '../actions/expenses';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 const ExpenseListItem = (props) => {
   return (
     <div>
       <h3>{props.description}</h3>
       <p>{props.amount} - {props.createdAt}</p>
+      <button onClick={e => props.removeExpense({ id: props.id })}>Remove</button>
     </div>
   )
 }
 
-export default ExpenseListItem;
+const mapStateToProps = (state) => {
+  return {
+    expenses: state.expenses
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ removeExpense }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListItem);
