4cb6836f4e71f762753831af652e6c01f2df12a9
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-01-06 12:34:00 +0800

    12-114: Testing Filters Action Generators

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,48 @@
+import moment from 'moment';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../../actions/filters';
+
+test('Should generate set start date action object', () => {
+  const action = setStartDate(moment(0));
+  expect(action).toEqual({
+    type: 'SET_START_DATE',
+    startDate: moment(0)
+  })
+});
+
+test('Should generate set end date action object', () => {
+  const action = setEndDate(moment(0));
+  expect(action).toEqual({
+    type: 'SET_END_DATE',
+    endDate: moment(0)
+  });
+});
+
+test('Should generate sort by date action object', () => {
+  const action = sortByDate();
+  expect(action).toEqual({
+    type: 'SORT_BY_DATE'
+  });
+});
+
+test('Should generate sort by amount action object', () => {
+  const action = sortByAmount();
+  expect(action).toEqual({
+    type: 'SORT_BY_AMOUNT'
+  });
+});
+
+test('Should generate set text filter with given text', () => {
+  const action = setTextFilter('Bill');
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER', 
+    text: 'Bill'
+  });
+});
+
+test('Should generate set text filter without giving the text param', () => {
+  const action = setTextFilter();
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: ''
+  });
+});
