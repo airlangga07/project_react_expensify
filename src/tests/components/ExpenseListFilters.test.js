b3be2a03bc1075f02cabef12019412bff1dfecaa
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-01-10 23:04:45 +0800

    12-127: Testing ExpenseListFilters Part 2

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -28,8 +28,39 @@ test('Should render ExpenseListFilters correctly', () => {
 });
 
 test('Should render ExpenseListFilter with altFilters', () => {
-  wrapper.setProps({
-    filters: altFilters
-  });
+  wrapper.setProps({ filters: altFilters });
   expect(wrapper).toMatchSnapshot();
+});
+
+test('Should handle text change', () => {
+  wrapper.find('input').simulate('change', {
+    target: { value: altFilters.text }
+  });
+  expect(setTextFilter).toHaveBeenLastCalledWith(altFilters.text);
+});
+
+test('Should sort by date', () => {
+  wrapper.find('select').simulate('change', {
+    target: { value: filters.sortBy }
+  });
+  expect(sortByDate).toHaveBeenCalled();
+})
+
+test('Should sort by amount', () => {
+  wrapper.find('select').simulate('change', {
+    target: { value: altFilters.sortBy }
+  });
+  expect(sortByAmount).toHaveBeenCalled();
+});
+
+test('Should handle date changes', () => {
+  wrapper.find('withStyles(DateRangePicker)').prop('onDatesChange')({ startDate: altFilters.startDate, endDate: altFilters.endDate });
+  expect(setStartDate).toHaveBeenLastCalledWith(altFilters.startDate);
+  expect(setEndDate).toHaveBeenLastCalledWith(altFilters.endDate);
+});
+
+test('Should handle date focus changes', () => {
+  const calendarFocused = 'endDate';
+  wrapper.find('withStyles(DateRangePicker)').prop('onFocusChange')(calendarFocused);
+  expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 });
