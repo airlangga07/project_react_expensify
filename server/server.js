f078c6d83b5b5606303383ef270682cb58fdad6d
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-01-15 21:46:15 +0800

    13-136: Deploying to Heroku: Setup production build and server

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const app = express();
 const path = require('path');
 const publicPath = path.join(__dirname, '..', 'public');
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));  
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("server is up!");
 });
