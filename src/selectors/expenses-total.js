a75d9024e25a7619bea6e03c649901c2f7af6e2e
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-01-16 23:12:08 +0800

    13-139: Build It: Adding Total Selector

---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,9 @@
+import moment from 'moment';
+
+const selectExpensesTotal = (expenses) => {
+  return expenses
+    .map(expense => expense.amount)
+    .reduce((sum, value) => sum + value, 0);
+} 
+
+export default selectExpensesTotal;
