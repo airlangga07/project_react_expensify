0e3afe4cfa72750f7893a2ce9a42ff18acfba0e2
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-01-21 16:36:41 +0700

    14-146: Removing Data from Database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -27,15 +27,24 @@ database.ref().set({
   console.log('Error: ' + error);
 });
 
+// update the data
 // database.ref('age').set(27);
-
 // database.ref('location/city').set('Balestier');
+// database.ref('attributes').set({
+//   height: 175,
+//   weight: 61
+// }).then(() => {
+//   console.log('Data is saved!');
+// }).catch(error => {
+//   console.log('Error: ' + error);
+// });
 
-database.ref('attributes').set({
-  height: 175,
-  weight: 61
-}).then(() => {
-  console.log9('Data is saved!');
-}).catch(error => {
-  console.log('Error: ' + error);
+// remove property from database
+database.ref('isSingle').remove().then(() => {
+  console.log('Data was removed');
+}).catch(err => {
+  console.log('Error: ' + err);
 });
+
+// by passing up the null value is equivalent by calling the remove
+// database.ref('isSingle').set(null);
