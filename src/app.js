36e96de9d5ea22ed07a93a8849af794836fef053
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-01-29 23:29:16 +0800

    16-164: Redirecting Login or Logout

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import 'react-dates/initialize';
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses, addExpense, removeExpense, editExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
@@ -30,16 +30,26 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+      ReactDOM.render(jsx, document.getElementById('app'));
+      hasRendered = true;
+  }
+};
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
-});
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
-    console.log('log in');
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
   } else {
-    console.log('log out');
+    renderApp();
+    history.push('/');
   }
 });
