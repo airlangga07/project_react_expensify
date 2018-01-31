c4188a2fd29865d58cfa32c756c0ce5fee8c5b75
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-01-31 23:36:12 +0800

    16-166: Private Only Routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import Header from '../components/Header';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
@@ -15,12 +15,11 @@ const AppRouter = () => {
   return (
     <Router history={history}>
       <div>
-        <Header />
         <Switch>
           <Route exact path="/" component={LoginPage} />
-          <Route path="/dashboard" component={ExpenseDashboardPage} />
-          <Route path="/create" component={AddExpensePage} />
-          <Route path="/edit/:id" component={EditExpensePage} />
+          <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+          <PrivateRoute path="/create" component={AddExpensePage} />
+          <PrivateRoute path="/edit/:id" component={EditExpensePage} />
           <Route path="/help" component={HelpPage} />
           <Route component={NotFoundPage} />
         </Switch>
