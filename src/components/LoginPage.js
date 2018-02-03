7fdba7a080bde23aca29ff429efae755200ba6ce
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2018-02-03 14:34:15 +0800

    17-172: Styling Buttons

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,9 +6,9 @@ export const LoginPage = (props) => {
   return (
     <div className="box-layout">
       <div className="box-layout__box">
-        <h1 className="box-layout__title">Expensify App</h1>
+        <h1 className="box-layout__title">Expensify</h1>
         <p>Its time to get your expenses under control.</p>
-        <button onClick={props.startLogin}>Login</button>
+        <button className="button" onClick={props.startLogin}>Login with Google</button>
       </div>
     </div>
   )
